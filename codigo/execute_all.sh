ea1ca1cfb5a0f3d9176723a26f86fcc0661c8397
Fernando Dias <fernando.dias.3741@gmail.com>
2025-01-05 10:14:04 -0300

    Script para executar todos os códigos na ordem

---
+++ codigo/execute_all.sh
@@ -0,0 +1,9 @@
+python3 utils.py
+python3 questao_2.py
+python3 questao_3.py 
+python3 questao_4.py False
+python3 questao_4.py True 
+python3 questao_5.py False
+python3 questao_5.py True 
+python3 questao_6.py False
+python3 questao_6.py True 
